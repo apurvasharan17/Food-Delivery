9c5b7027697edae8b798dd3815d16f93c14890c2
Apurva Sharan <ranjan.apurva2017@gmail.com>
2024-07-04 19:37:46 +0530

    react router dom

--- src/app.js
+++ src/app.js
@@ -2,46 +2,47 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header.js";
 import Body from "./components/Body.js";
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import About from "./components/About.js";
+import Contact from "./components/Contact.js";
+import Error from "./components/Error.js";
 
 // const styleCard={
 //         backgroundColor:"#f0f0f0",
 // }
-
-const RestaurantCard=(props)=>{
-        const { resData } = props;
-
-  const { name, cuisines, costForTwo,cloudinaryImageId, avgRating} =
-    resData?.info;
-
-        
-       return(
-        <div className="res-card" style={
-                {backgroundColor:"#f0f0f0"}}>
-                        <img 
-                        className="res-logo"
-                        alt="res-logo"
-                        src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId}/>
-                        <h3>{name}</h3>
-                        <h4>{cuisines.join(", ")}</h4>
-                        <h4>Rating: {avgRating}</h4>
-                        <h4>{costForTwo}</h4>
-        </div>
-       ) 
-}
-
-
 const AppLayout=()=>{
         return(
                 <div className="app">
                       <Header/>
-                      <Body/>
+                      <Outlet />
                 </div>
-        )
-}
+        );
+};
+const appRouter=createBrowserRouter([
+        {
+                path:"/",
+                element:<AppLayout/>,
+                children:[{
+                                path:"/",
+                                element:<Body/>,
 
+                           },
+                        {
+                                path:"/about",
+                                element:<About/>,
+                        },{
+                                path:"/contact",
+                                element:<Contact/>,
+                        },
+                ]
+                ,
+                errorElement:<Error/>,
+        },
+]);
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>);
+root.render(<RouterProvider router={appRouter}/>);
 
 
 
